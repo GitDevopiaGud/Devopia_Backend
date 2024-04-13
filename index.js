ee9dd180b5c58c740e109115af70d39616b4e4ff
Chirag30Sharma <chirag30sharma@gmail.com>
2024-04-13 20:32:13 +0530

    Remove MCQ quiz route and related schema files

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 const authRouter = require('./routes/auth')
-const quizRouter = require('./routes/mcq')
 
 app.use('/auth', authRouter)
-app.use('/mcq', quizRouter)
 
 app.listen(process.env.PORT, () => { console.log(`Working On Port ${process.env.PORT}`) })
