3bde6c6a897b7a39aaa23abb4488aa95ee9e137b
Chirag30Sharma <chirag30sharma@gmail.com>
2024-04-13 11:21:59 +0530

    Add dependencies bcrypt, cors, dotenv, and mongoose to package.json Update index.js to include express, dotenv, cors, and body-parser

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+require('dotenv').config()
+const cors = require("cors");
+const bodyParser = require('body-parser')
+require('./connection')
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+const authRouter = require('./routes/auth')
+
+app.use('/auth', authRouter)
+
+app.listen(process.env.PORT, () => { console.log(`Working On Port ${process.env.PORT}`) })
